e4d36388ccfab1c2a654617a7955d0e054a31be1
25LP7053 25LP7053 <johndoe@example.com>
2025-12-12 10:05:09 +0800

    ehniih

---
+++ src/components/todo.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+export const todo = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  function inputChange(event) {}
+};
+export default todo;
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/Header";
+
+const index = () => {
+  return (
+    <div className="bg-gray-400 flex justify-center h-250 items-center">
+      <Header />
+    </div>
+  );
+};
+
+export default index;
